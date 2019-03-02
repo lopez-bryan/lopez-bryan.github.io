b414b17da71be8bedc0fc2bace9955c2fd352e85
Bryan Lopez <bryanjlopez01@gmail.com>
2019-03-01 22:21:18 -0800

    Update service-worker.js
    Adding links to sw.

--- pages/final-app/service-worker.js
+++ pages/final-app/service-worker.js
@@ -1,24 +1,24 @@
 var cacheName = 'v1';
 var cacheFiles = [
-	'index1.html',
-	'css/main.css',
-	'css/planets.css',
-	'images/earth.jpg',
-	'images/jupiter.png',
-	'images/mars.jpg',
-	'images/venus.jpg',
-	'images/Uranus.jpg',
-	'images/saturn.jpg',
-	'images/neptune.jpg',
-	'images/mercury.jpg',
-	'videos/61_2012VenusTransit~small.mp4',
-	'videos/215_TransitOfMercury~small.mp4',
-	'videos/269_NewScienceFromJupiter~small.mp4',
-	'videos/Earth-Observations~small.mp4',
-	'videos/First-Spacecraft-Neptune~small.mp4',
-	'videos/First-Spacecraft-Uranus~small.mp4',
-	'videos/JPL-20180220-MARSf-0001-Whats Inside Mars~small.mp4',
-	'videos/Saturn_Rings~small.mp4'];
+	'/index1.html',
+	'/css/main.css',
+	'/css/planets.css',
+	'/images/earth.jpg',
+	'/images/jupiter.png',
+	'/images/mars.jpg',
+	'/images/venus.jpg',
+	'/images/Uranus.jpg',
+	'/images/saturn.jpg',
+	'/images/neptune.jpg',
+	'/images/mercury.jpg',
+	'/videos/61_2012VenusTransit~small.mp4',
+	'/videos/215_TransitOfMercury~small.mp4',
+	'/videos/269_NewScienceFromJupiter~small.mp4',
+	'/videos/Earth-Observations~small.mp4',
+	'/videos/First-Spacecraft-Neptune~small.mp4',
+	'/videos/First-Spacecraft-Uranus~small.mp4',
+	'/videos/JPL-20180220-MARSf-0001-Whats Inside Mars~small.mp4',
+	'/videos/Saturn_Rings~small.mp4'];
 
 
 self.addEventListener('install', function(e) {
