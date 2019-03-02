5313314f35fbe703043f2cde47e74b032e2001ad
Bryan Lopez <bryanjlopez01@gmail.com>
2019-03-01 22:16:56 -0800

    Update app.js
    update

--- pages/final-app/js/app.js
+++ pages/final-app/js/app.js
@@ -1,9 +1,23 @@
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker
-		.register('https://lopez-bryan.github.io/pages/final-app/service-worker.js').then(function(registration) {
-			console.log("Service Worker Registered");
-		})
-		.catch(function(err) {
-			console.log("Service Worker Failed to Register", err);
-		});
+
+  navigator.serviceWorker
+    .register('./service-worker.js', { scope: './' })
+    .then(function(registration) {
+      console.log("Service Worker Registered");
+    })
+    .catch(function(err) {
+      console.log("Service Worker Failed to Register", err);
+    })
+
 }
+
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker
+// 		.register('https://lopez-bryan.github.io/pages/final-app/service-worker.js').then(function(registration) {
+// 			console.log("Service Worker Registered");
+// 		})
+// 		.catch(function(err) {
+// 			console.log("Service Worker Failed to Register", err);
+// 		});
+// }
+
